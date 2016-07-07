c0853afa92f2f6d56acf5c810daab4db4f062330
Tristan Gordon <tgordon@athlonsports.com>
2016-07-07 14:35:25 -0500

    split todo.html into todo.js, todo.css, and todo.html

--- todo.js
+++ todo.js
@@ -2,12 +2,21 @@ document.addEventListener("DOMContentLoaded", function () {
 	var todo = Object.create(null);
 	var ul = document.getElementById("todo-view");
 	var input = document.getElementById("todo-item");
+	var editing = false;
+	var count = 0;
 
 	todo.newTodo = function (evt) {
 		evt.preventDefault();
+
+		if (count >= 1000) {
+
+			return;
+
+		}
+
 		var newTodo = input.value;
 		var li = document.createElement("li");
-		li.innerHTML = '<input type="checkbox">' + newTodo + '<i class="material-icons">close</i>';
+		li.innerHTML = '<input type="checkbox" class="checkbox">' + '<p>' + newTodo + '</p>' + '<i class="material-icons">close</i>';
 
 		if (!newTodo.replace(/\s/g, '').length) {
 			input.value = "";
@@ -26,6 +35,12 @@ document.addEventListener("DOMContentLoaded", function () {
 			var checkbox = li.getElementsByTagName("input");
 
 			checkbox[0].addEventListener("click", todo.finishedTodo, false);
+
+			li.childNodes[1].addEventListener("dblclick", todo.editTodo, false);
+
+			count++;
+
+			console.log(count);
 		}
 	}
 
@@ -39,7 +54,8 @@ document.addEventListener("DOMContentLoaded", function () {
 		return todoCache;
 	}
 
-	todo.undoDelete = function () {
+	todo.undoDelete = function (evt) {
+		evt.preventDefault();
 
 		if (todoCache.length > 0 ) {
 			var lastTodo = todoCache.length - 1;
@@ -54,19 +70,121 @@ document.addEventListener("DOMContentLoaded", function () {
 		}
 	}
 
-
-
 	todo.finishedTodo = function () {
 		//using ternary operator to check for finished todo items
 		(this.checked) ? this.parentNode.classList.add('line-through') : this.parentNode.classList.remove('line-through');
 
 	}
 
-	var todoSub = document.getElementById("submit-item");
+	todo.editTodo = function () {
 
-	todoSub.addEventListener("click", todo.newTodo, false);
+		if (editing == true) {
+			return;
+		}
 
-	var undo = document.getElementById('undo'); 
+		else {
+			var todoVal = this.innerHTML;
+			var input = document.createElement('input');
 
-	undo.addEventListener("click", todo.undoDelete, false);
+			editing = true;
+
+			input.value = todoVal;
+			this.innerHTML = '';
+			this.appendChild(input);
+
+			input.addEventListener("keydown", todo.saveEdit, false);
+
+			input.parentNode.parentNode.addEventListener("click", todo.saveEditAlt, false);
+
+			input.addEventListener("click", function (evt) { evt.stopPropagation();} , false);
+		}
+
+	}
+
+	todo.saveEdit = function (e) {
+
+		var newTodo = this.value;
+		if (!newTodo.replace(/\s/g, '').length) {
+			this.value = '';
+
+			return;
+		}
+
+		if (e.keyCode == 13) this.parentNode.innerHTML = newTodo;
+
+		editing = false;
+
+	}
+
+	todo.saveEditAlt = function (e) {
+		e.stopPropagation();
+
+		var newTodo = this.childNodes[1].childNodes[0].value || '';
+		
+		if (!newTodo.replace(/\s/g, '').length) {
+			this.value = '';
+
+			return;
+		}
+
+		this.childNodes[1].innerHTML = newTodo;
+
+		editing = false;
+
+	}
+
+	todo.deleteCheckedTodo = function () {
+		var finishedTodos = document.getElementsByClassName('line-through');
+
+		if (finishedTodos.length == 0) return;
+
+		while (0 < finishedTodos.length) {
+			var i = finishedTodos.length;
+			todoCache.push(finishedTodos[i-1]);
+			finishedTodos[i-1].parentNode.removeChild(finishedTodos[i-1]);
+		}
+
+		document.getElementById("check-all").checked = false;
+	}
+
+	todo.checkAllTodo = function () {
+
+		if (!document.getElementsByClassName('checkbox').length) {
+
+			document.getElementById("check-all").checked = false;
+
+			return;
+
+		}
+
+		var checkboxes = document.getElementsByClassName('checkbox');
+
+		if (document.getElementById("check-all").checked == true) {
+			for (i = 0; i < checkboxes.length; i++) {
+				checkboxes[i].checked = true;
+				checkboxes[i].parentNode.classList.add('line-through');
+			}
+
+			console.log('its lit');
+		}
+		else {
+			for (i = 0; i < checkboxes.length; i++) {
+				checkboxes[i].checked = false;
+				checkboxes[i].parentNode.classList.remove('line-through');
+			}
+
+			console.log('well, at least it WAS lit, fam.');
+		}
+	}
+
+	document.getElementById("submit-item").addEventListener("click", todo.newTodo, false);
+
+	document.getElementById("check-all").addEventListener("click", todo.checkAllTodo, false);
+
+	document.getElementById("check-all").checked = false;
+
+	document.getElementById('undo').addEventListener("click", todo.undoDelete, false);
+
+	document.getElementById('delete-finished').addEventListener("click", todo.deleteCheckedTodo, false);
+
 });
